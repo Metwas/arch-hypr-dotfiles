fa9c25e71ba30b07b6ba2150ed0d0daf8a999e26
metwas <metwas.seup@gmail.com>
2025-12-26 11:13:49 +0000

    Update hyprland-dotfiles.sh

--- scripts/hyprland-dotfiles.sh
+++ scripts/hyprland-dotfiles.sh
@@ -3,7 +3,7 @@
 curl -L -O https://github.com/Metwas/arch-hypr-dotfiles/archive/refs/heads/main.zip
 sudo unzip ./main.zip
 sudo cp -r ./arch-hypr-dotfiles-main/* ./
-sudo cp -f ./arch-hypr-dotfiles-main/.* ./
+sudo cp -r ./arch-hypr-dotfiles-main/.* ./
 
 sudo rm -rf ./arch-hypr-dotfiles-main
 sudo rm ./main.zip
